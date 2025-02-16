073f0aea3222bbd409b5681b2a35cb5b24641405
Veekshith-Thoram <67872770+Veekshith-Thoram@users.noreply.github.com>
2025-02-15 22:48:40 -0600

    function as a first class citizen

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("Hey Thalli")
+
+
+//functions assigned to variables
+const printName =  nameLength => console.log(`OMG my name is ${nameLength} long`);
+printName(4);
+
+//functions in objects
+const Obj = {};
+Obj.printName = nameL => console.log(`Omg my name is ${nameL} Long!`);
+Obj.printName(5);
+
+//functions assigned in arrays
+const arr = ["1", "2", nameLength => console.log(`Omg my name is ${nameLength} Long!`)];
+arr[2](6);
